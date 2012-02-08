e696b481c4e30273bc49ea4cfc57d74d12461695
Richard Walton <walton909@gmail.com>
2012-02-08 01:11:06 +0000

    Updated tests

--- src/richTea/test/AttributeTests.java
+++ src/richTea/test/AttributeTests.java
@@ -2,12 +2,16 @@ package richTea.test;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 
 import richTea.core.attribute.Attribute;
+import richTea.core.attribute.FunctionAttribute;
+import richTea.core.attribute.VariableAttribute;
 
-public class AttributeTest extends NodeBuilderTestBase {
+public class AttributeTests extends NodeBuilderTestBase {
 	
 	@Test
 	public void testCreateNumberAttribute() throws RecognitionException {
@@ -35,5 +39,29 @@ public class AttributeTest extends NodeBuilderTestBase {
 		Attribute attribute = buildAttribute("x=true ? false : true");
 		
 		assertTrue(attribute.getValue().equals(false));
+	}
+	
+	@Test
+	public void testListAttribute() throws RecognitionException {
+		Attribute attribute = buildAttribute("x=[1,2,3,4]");
+		
+		Object value = attribute.getValue();
+		
+		assertTrue(value instanceof List<?>);
+		assertTrue(((List<?>) value).size() == 4);
+	}
+	
+	@Test
+	public void testVariableAttribute() throws RecognitionException {
+		Attribute attribute = buildAttribute("x=var");
+		
+		assertTrue(attribute instanceof VariableAttribute);
+	}
+	
+	@Test
+	public void testFunctionAttribute() throws RecognitionException {
+		Attribute attribute = buildAttribute("x=Scope()");
+				
+		assertTrue(attribute instanceof FunctionAttribute);
 	}
 }
--- src/richTea/test/BranchTests.java
+++ src/richTea/test/BranchTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 
+import richTea.core.node.Branch;
 import richTea.core.node.TreeNode;
 
 public class BranchTests extends NodeBuilderTestBase {
@@ -18,30 +19,47 @@ public class BranchTests extends NodeBuilderTestBase {
 	
 	@Test
 	public void testImplicitBranch() throws RecognitionException {
-		TreeNode node = buildNode("({})");
+		TreeNode node = buildNode("(#{})");
 		
 		assertTrue(node.getBranches().length == 1);
 	}
 	
 	@Test
 	public void testExplicitBranch() throws RecognitionException {
-		TreeNode node = buildNode("( branch {} )");
+		TreeNode node = buildNode("( #branch {} )");
 		
 		assertTrue(node.getBranches().length == 1);
 	}
 	
 	@Test
 	public void testMixedBranches() throws RecognitionException {
-		TreeNode node = buildNode("( {} branch {} )");
+		TreeNode node = buildNode("( #{} #branch {} )");
 		
 		assertTrue(node.getBranches().length == 2);
 	}
 	
 	@Test
 	public void testDuplicateBranches() throws RecognitionException {
-		TreeNode node = buildNode("( branch {} branch {} branch {})");
+		TreeNode node = buildNode("( #branch {} #branch {} #branch {})");
 		
-		assertTrue(node.getBranches().length == 2);
+		assertTrue(node.getBranches().length == 1);
+	}
+	
+	@Test
+	public void testBranchName() throws RecognitionException {
+		TreeNode node = buildNode("( #myBranch {})");
+		
+		assertTrue(node.hasBranch("myBranch"));
+	}
+	
+	@Test
+	public void testBranchChildren() throws RecognitionException {
+		TreeNode node = buildNode("( #myBranch { (); () })");
+		
+		Branch branch = node.getBranch("myBranch");
+		
+		
+		assertTrue(branch.getChildren().length == 2);
 	}
 
 }
