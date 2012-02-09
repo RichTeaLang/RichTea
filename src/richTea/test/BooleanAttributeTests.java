46d50a4cd92157d0ae517e5e4a3fa941c00d7224
Richard Walton <walton909@gmail.com>
2012-02-09 00:38:52 +0000

    Added final boolean attribute tests

--- src/richTea/test/BooleanAttributeTests.java
+++ src/richTea/test/BooleanAttributeTests.java
@@ -48,4 +48,26 @@ public class BooleanAttributeTests extends NodeBuilderTestBase {
 		testAttribute("x=1 < 1", false);
 		testAttribute("x=0 < 1", true);
 	}
+	
+	@Test
+	public void testEqualsAttribute() throws RecognitionException {
+		testAttribute("x=1 == 0", false);
+		testAttribute("x=1 == 1", true);
+		testAttribute("x=0 == 1", false);
+		testAttribute("x=\"Hello\" == \"Hello\"", true);
+	}
+	
+	@Test
+	public void testNotEqualsAttribute() throws RecognitionException {
+		testAttribute("x=1 != 0", true);
+		testAttribute("x=1 != 1", false);
+		testAttribute("x=0 != 1", true);
+		testAttribute("x=\"Hello\" != \"Hello\"", false);
+	}
+	
+	@Test
+	public void testNotAttribute() throws RecognitionException {
+		testAttribute("x=!true", false);
+		testAttribute("x=!false", true);
+	}
 }
