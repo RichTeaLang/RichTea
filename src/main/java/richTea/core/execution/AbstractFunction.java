11a1585699aecc1f9040d77d3adb7487d2b2541a
Richard Walton <walton909@gmail.com>
2012-08-13 00:59:48 +0100

    All functions now support a special "id" attribute which, if set, takes the last returned value after the function has been executed and adds it as a new variable (using the value of the "id" attribute as the attribute key) to the variable scope.

--- src/main/java/richTea/core/execution/AbstractFunction.java
+++ src/main/java/richTea/core/execution/AbstractFunction.java
@@ -2,6 +2,8 @@ package richTea.core.execution;
 
 import org.apache.log4j.Logger;
 
+import richTea.core.attribute.PrimativeAttribute;
+
 public abstract class AbstractFunction implements RichTeaFunction {
 
 	protected Logger log = Logger.getLogger(getClass());
@@ -13,10 +15,21 @@ public abstract class AbstractFunction implements RichTeaFunction {
 	public void execute(ExecutionContext context) {
 		this.context = context;
 		
-		
 		if(shouldExecute()) {
 			try {
 				run();
+				
+				String id = context.getString("id");
+				
+				if(id != null) {
+					VariableScope scope = context.getCurrentScope().getParent();
+					
+					if(scope != null) {
+						scope.setAttribute(new PrimativeAttribute(id, context.getLastReturnValue()));
+					} else {
+						throw new IllegalArgumentException("No scope to create variable");
+					}
+				}
 			}catch(RuntimeException runtimeException) {
 				throw runtimeException;
 			}catch(Exception checkedExceception) {
