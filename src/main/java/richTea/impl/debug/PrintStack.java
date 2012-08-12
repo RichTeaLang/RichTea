e884f869b86f218cf1f667943b058c09aa0fabd9
Richard Walton <walton909@gmail.com>
2012-08-12 12:24:29 +0100

    Updated print stack to provide more useful information.

--- src/main/java/richTea/impl/debug/PrintStack.java
+++ src/main/java/richTea/impl/debug/PrintStack.java
@@ -1,5 +1,6 @@
 package richTea.impl.debug;
 
+import richTea.core.attribute.Attribute;
 import richTea.core.execution.AbstractFunction;
 import richTea.core.execution.VariableScope;
 
@@ -12,19 +13,28 @@ public class PrintStack extends AbstractFunction {
 		String indent = "";
 		
 		while(scope != null) {
-			println(scope.getOwner().getFunction().getClass().getSimpleName(), indent);
+			println("[F] " + scope.getOwner().getFunction().getClass().getSimpleName(), indent);
 			
 			indent += " ";
 			
-			println(scope.getAttributes(), indent);
-			println(scope.getOwner().getAttributes(), indent);
+			printAttributeList(scope, indent);
 			
 			scope = scope.getParent();
+		}
+	}
+	
+	protected void printAttributeList(VariableScope scope, String indent) {
+		
+		for(Attribute attribute : scope.getAttributes()) {
+			println("[A] " + attribute.getName(), indent);
+		}
+		
+		for(Attribute attribute : scope.getOwner().getAttributes()) {
+			println("[A] " + attribute.getName(), indent);
 		}
 	}
 	
 	private void println(Object message, String indent) {
 		System.out.println(indent + message);
 	}
-
 }
