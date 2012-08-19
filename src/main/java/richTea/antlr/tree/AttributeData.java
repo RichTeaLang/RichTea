1795a384c6d5964a8cc2cd211bfb4b5a227e5db9
Richard Walton <walton909@gmail.com>
2012-08-19 22:14:38 +0100

    Added basic "evaluate" function which currently is able to evaluate strings containing RichTea attribute declarations.  E.g Eval("10+10) == 20.

--- src/main/java/richTea/antlr/tree/AttributeData.java
+++ src/main/java/richTea/antlr/tree/AttributeData.java
@@ -10,6 +10,13 @@ public class AttributeData extends RichTeaTree {
 	public AttributeData(Token token) {
 		super(token);
 	}
+	
+	public AttributeData(Tree data) {
+		super(null);
+		
+		addChild(data.getChild(0));
+		addChild(data.getChild(1));
+	}
 			
 	public Tree getValue() {
 		return getChild(VALUE_CHILD_INDEX).getChild(0);
