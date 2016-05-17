c1487023c4b05b5dc421322ce789f709bd32526b
Richard Walton <walton909@gmail.com>
2016-05-18 00:35:46 +0100

    Moved "Return" from stdlib into core.
     - Return relies on ReturnException which has to be in core, as Scope catches these exceptions.

---
+++ src/main/java/richTea/impl/Return.java
@@ -0,0 +1,19 @@
+package richTea.impl;
+
+import richTea.core.execution.AbstractFunction;
+import richTea.core.execution.ReturnException;
+
+public class Return extends AbstractFunction {
+
+	@Override
+	protected void run() throws Exception {
+		context.setLastReturnValue(getReturnValue());
+		
+		// This runtime exception should be handled by a Scope function causing execution to jump outside of that scope
+		throw new ReturnException(context.getCurrentScope());
+	}
+	
+	protected Object getReturnValue() {
+		return context.getValue("value");
+	}
+}
